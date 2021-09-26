55258fae3ad70bb2e1f382fd16e77da6f5b3c622
PiyushSinha-9 <piyush7924@gmail.com>
2021-09-26 18:38:38 +0530

    Graphs Algorithms Added

---
+++ CodeForces_CodeChef/Contant/CodeChef-Luchtime-Sep-2021/temp.py
@@ -0,0 +1,5 @@
+for _ in range(int(input())):
+    a,b=list(map(int, input().split()))
+    at = a//3
+    bt = b//3
+    print(at-bt)
---
+++ CodeForces_CodeChef/Contant/CodeChef-Luchtime-Sep-2021/temp3.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<long long,long long> pll;
+
+#define pi (3.141592653589)
+#define mod 1000000007
+#define F first
+#define S second
+#define print(x) for(auto iuiuiuiuiui:x){ cout<<iuiuiuiuiui<<" ";}br;
+#define print_array(array,size) for(ll i=0;i<size;i++){ cout<<array[i]<<" ";}br;
+#define br cout<<endl
+#define pb(a) push_back(a)
+#define mp(asd,fgh) make_pair(asd,fgh)
+#define all(c) c.begin(),c.end()
+#define ff first
+#define ss second
+#define min3(a, b, c) min(c, min(a, b))
+#define max3(a, b, c) max(c, max(a, b))
+#define min4(a, b, c, d) min(d, min(c, min(a, b)))
+#define rrep(i, n) for(int i=n-1;i>=0;i--)
+#define rep(i,n) for(int i=0;i<n;i++)
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+#define FILE_READ_IN freopen("input.txt","r",stdin);
+#define FILE_READ_OUT freopen("output.txt","w",stdout);
+
+#define INF LONG_LONG_MAX
+#define N_INF LONG_LONG_MIN
+
+#pragma GCC target("popcnt")
+
+//############################################################
+
+
+
+bool divisorGame(int N, bool A, int dp[][2])
+{
+
+
+	if (N == 1 or N == 3)
+		return false;
+
+	if (N == 2)
+		return true;
+
+	if (dp[N][A] != -1)
+		return dp[N][A];
+
+	int ans = (A == 1) ? 0 : 1;
+
+	for (int i = 1; i * i <= N; i++) {
+		if (N % i == 0) {
+			if (A)
+				ans |= divisorGame(N - i, 0, dp);
+			else
+				ans &= divisorGame(N - i, 1, dp);
+		}
+	}
+	return dp[N][A] = ans;
+}
+
+
+signed main() {
+    fast
+    #ifndef ONLINE_JUDGE
+    FILE_READ_IN
+    FILE_READ_OUT
+    #endif
+
+    //######################
+
+    int N = 3;
+
+	int dp[N + 1][2];
+
+	memset(dp, -1, sizeof(dp));
+
+	if (divisorGame(N, 1, dp) == true)
+		cout << "Player A wins";
+	else
+		cout << "Player B wins";
+
+	return 0;
+}
+
